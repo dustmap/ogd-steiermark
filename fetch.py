c6e06c1c1868ebb16244c4a4798beb3833e9b62c
Erwin Nindl <nine@wirdorange.org>
2013-06-08 17:05:17 +0200

    created json output

--- fetch.py
+++ fetch.py
@@ -1,8 +1,12 @@
-
+import os
 import urllib
 import subprocess 
 from pyExcelerator import *
+import simplejson as json
+from datetime import datetime
+import pytz
 
+node_id = '1234'
 url = 'http://app.luis.steiermark.at/luft2/export.php'
 values = {
     'station1' : '138',
@@ -21,14 +25,15 @@ values = {
   }
 data = urllib.urlencode(values)
 data = data.encode('utf-8') # data should be bytes
+local_filename, headers = urllib.urlretrieve(url, '/run/shm/ogd-graz.xls', None, data)
 
-local_filename, headers = urllib.urlretrieve(url, '/tmp/foo2.xls', None, data)
-print local_filename
+
+data = {}
 
 for sheet_name, values in parse_xls(local_filename, 'cp1251'):
   matrix = [[]]
-  print 'Sheet = "%s"' % sheet_name.encode('cp866', 'backslashreplace')
-  print '----------------'
+  print '"%s"' % sheet_name.encode('cp866', 'backslashreplace')
+  print ''
   for row_idx, col_idx in sorted(values.keys()):
       v = values[(row_idx, col_idx)]
       if isinstance(v, unicode):
@@ -46,8 +51,24 @@ for sheet_name, values in parse_xls(local_filename, 'cp1251'):
 
       matrix[-1].extend([v])
 
-  for row in matrix:
-      csv_row = ','.join(row)
-      print csv_row
+  for idx,row in enumerate(matrix):
+    # skip xls-header
+    if idx<4:
+      continue
+    csv_row = ','.join(row)
+    #print csv_row
+    # convert date
+    date_str = row[0] + row[1]
+    date = datetime.strptime(date_str, '%d.%m.%y%H:%M')
+    local = pytz.timezone("Europe/Vienna")
+    local_dt = local.localize(date, is_dst=None)
+    date_utc = local_dt.astimezone(pytz.utc)
+    timestamp_utc = date_utc.strftime("%s")
+    #print date_utc.strftime("%Y-%m-%d %H:%M:%S")
+    data.update( { timestamp_utc : [ { 'value' : row[2], 'type' : 'dust_concentration' } ] } ) 
 
+os.remove(local_filename)
 
+print json.dumps({ node_id: data })
+
+#eof
